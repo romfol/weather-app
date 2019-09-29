435e840727dc0c974fff7fda9b784e10ca926435
romfol <romanfoltyn1@gmail.com>
2019-09-29 22:27:47 +0300

    get data on frontend

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
 console.log("JS loaded");
+
+fetch("http://localhost:3000/weather?address=amsterdam").then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      return console.log(data.error);
+    }
+    console.log(data);
+  })
+})
