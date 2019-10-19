f0ba37851f7cc39ea1858a7ce24dcdc1fd8da02a
romfol <romanfoltyn1@gmail.com>
2019-10-19 23:40:57 +0300

    handling search input

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,16 @@
-console.log("JS loaded");
+console.log('JS loaded');
 
-fetch("http://localhost:3000/weather?address=amsterdam").then((response) => {
-  response.json().then((data) => {
+fetch('http://localhost:3000/weather?address=amsterdam').then(response => {
+  response.json().then(data => {
     if (data.error) {
       return console.log(data.error);
     }
     console.log(data);
-  })
-})
+  });
+});
+
+const weatherForm = document.querySelector('form');
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(111);
+});
