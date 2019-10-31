111559b08e56d96229c96b34cc4ec8d0e32fa65a
romfol <romanfoltyn1@gmail.com>
2019-10-31 21:48:46 +0200

    styled FED

--- public/js/app.js
+++ public/js/app.js
@@ -3,15 +3,21 @@ console.log('JS loaded');
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
+const paragraph1 = document.querySelector('#paragraph-1');
+const paragraph2 = document.querySelector('#paragraph-2');
+
 weatherForm.addEventListener('submit', e => {
-  // e.preventDefault();
+  e.preventDefault();
+  paragraph1.textContent = 'Location: Loading...';
   const location = search.value;
   fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      return console.log(data.error);
-    }
-    console.log(data);
+    response.json().then(data => {
+      if (data.error) {
+        paragraph2.textContent = '';
+        return (paragraph1.textContent = 'Error: ' + data.error);
+      }
+      paragraph1.textContent = 'Location: ' + data.location;
+      paragraph2.textContent = 'Forecast: ' + data.forecast;
+    });
   });
-});
 });
